ec9ecbe50f48124a5705660d9c2723994a2c98f4
UnHa Kim <unha.kim.ghts@gmail.com>
2018-05-07 20:28:40 +0900

    t1310 전일/당일 일자 올바르게 생성됨.

--- internal/types_c_base_386.go
+++ internal/types_c_base_386.go
@@ -35,6 +35,7 @@ package xing_C32
 
 import (
 	"sync"
+	"time"
 	"unsafe"
 )
 
@@ -120,6 +121,47 @@ func (s *S메시지_저장소) S추가(식별번호 int, 메시지 unsafe.Pointe
 }
 
 func (s *S메시지_저장소) S삭제(식별번호 int) {
+	s.Lock()
+	defer s.Unlock()
+
+	delete(s.저장소, 식별번호)
+}
+
+func New콜백_대기_항목(식별번호 int, TR코드 string, 값 interface{}) *S콜백_대기_항목 {
+	s := new(S콜백_대기_항목)
+	s.M식별번호 = 식별번호
+	s.M생성_시각 = time.Now()
+
+	return s
+}
+
+type S콜백_대기_항목 struct {
+	M식별번호  int
+	M생성_시각 time.Time
+	TR코드   string
+	M값     interface{}
+}
+
+type S콜백_대기_저장소 struct {
+	sync.Mutex
+	저장소 map[int]*S콜백_대기_항목
+}
+
+func (s *S콜백_대기_저장소) G대기_항목(식별번호 int) *S콜백_대기_항목 {
+	s.Lock()
+	defer s.Unlock()
+
+	return s.저장소[식별번호]
+}
+
+func (s *S콜백_대기_저장소) S추가(식별번호 int, 대기_항목 *S콜백_대기_항목) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.저장소[식별번호] = 대기_항목
+}
+
+func (s *S콜백_대기_저장소) S삭제(식별번호 int) {
 	s.Lock()
 	defer s.Unlock()
 
